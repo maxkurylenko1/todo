4bfe6620d69f76eaf3c07bcf9dbf4394f71b5817
Max <maxkurylenko1@gmail.com>
2025-09-29 16:28:54 +0200

    hotfix/building issues

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles/reset.scss";
-import App from "./App.tsx";
-import { ToDoProvider } from "./context/ToDoContext.tsx";
+import App from "./App";
+import { ToDoProvider } from "./context/ToDoContext";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
